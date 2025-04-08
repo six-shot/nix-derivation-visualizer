4b15b633e983c074841065a9fa3e0afd9cfaec29
six-shot <okhuomonajayi54@gmail.com>
2025-04-08 20:35:20 +0100

    default.nix

--- nix-derivation-visualizer/default.nix
+++ nix-derivation-visualizer/default.nix
@@ -1,15 +1,15 @@
 let
-    src = ./nix-derivative-frontend/nix-derivative-frontend;
+  src = ./nix-derivative-frontend/nix-derivative-frontend;
 in
 buildNpmPackage {
-    pname = "frontend";
-    version = "0.0.0";
-    
-    inherit src; # Same as: `src = src;``
-    
-    npmDeps = importNpmLock {
-      npmRoot = src;
-    };
-    
-    npmConfigHook = importNpmLock.npmConfigHook;
+  pname = "frontend";
+  version = "0.0.0";
+
+  inherit src; # Same as: `src = src;``
+
+  npmDeps = importNpmLock {
+    npmRoot = src;
+  };
+
+  npmConfigHook = importNpmLock.npmConfigHook;
 }
