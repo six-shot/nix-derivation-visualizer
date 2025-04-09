ba5cda16395bf0ff237088601109dfe7acfaa230
six-shot <okhuomonajayi54@gmail.com>
2025-04-09 01:41:30 +0100

    Ignore the result directory inside nix-derivation-visualizer

--- nix-derivation-visualizer/convert_dot_to_json.py
+++ nix-derivation-visualizer/convert_dot_to_json.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 
+
 def convert_nix_to_graph(nix_expression):
     dot_file = "graph.dot"
     
@@ -38,6 +39,7 @@ def convert_nix_to_graph(nix_expression):
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/convert', methods=['POST'])
 def convert():
     if not request.json or 'nix_expression' not in request.json:
@@ -52,4 +54,5 @@ def convert():
     return jsonify(graph_data)
 
 if __name__ == '__main__':
+
     app.run(debug=True)
